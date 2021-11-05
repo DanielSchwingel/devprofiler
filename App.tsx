ece786a632bfdeef090462ebd80181e6e3e3916d
Daniel Filipe Schwingel <danielfilipeschwingel@gmail.com>
2021-11-04 21:31:05 -0300

    Added home screen

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { Login } from './src/screens/Login'
 
 function App() {
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+import { styles } from './styles'
+
+function Home() {
+   return (
+      <View style={styles.container}>
+         <Text>Teste</Text>
+      </View>
+   )
+}
+
+export { Home }
